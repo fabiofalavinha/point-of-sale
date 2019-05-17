ecde3e4c3907dff6acef03c474b84b980bfd2520
fabiofalavinha <fabio.falavinha@gmail.com>
2019-05-17 05:24:05 -0300

    enable mvc tests

--- src/test/java/com/ztech/codechallenge/backend/ZtechBackendApplicationTests.java
+++ src/test/java/com/ztech/codechallenge/backend/ZtechBackendApplicationTests.java
@@ -2,11 +2,13 @@ package com.ztech.codechallenge.backend;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebMvcTest
 public class ZtechBackendApplicationTests {
 
 	@Test
